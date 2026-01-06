c28042f7c79e8f2094a2505430f3e8f5f49bbb10
MeMayBeo <minhnhat20120504@gmail.com>
2026-01-06 10:38:02 +0700

    Create map.js

---
+++ map.js
@@ -0,0 +1,22 @@
+{
+  "length": 12000,
+  "objects": [
+    { "type": "block", "x": 0 },
+    { "type": "spike", "x": 600 },
+    { "type": "spike", "x": 950 },
+    { "type": "orb",   "x": 1400 },
+    { "type": "pad",   "x": 1800 },
+    { "type": "spike", "x": 2300 },
+    { "type": "spike", "x": 2700 },
+    { "type": "orb",   "x": 3200 },
+    { "type": "pad",   "x": 3600 },
+    { "type": "spike", "x": 4200 },
+    { "type": "spike", "x": 4800 },
+    { "type": "orb",   "x": 5500 },
+    { "type": "pad",   "x": 6000 },
+    { "type": "spike", "x": 7000 },
+    { "type": "spike", "x": 8000 },
+    { "type": "spike", "x": 9000 },
+    { "type": "spike", "x": 10500 }
+  ]
+}
